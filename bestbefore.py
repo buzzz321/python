854a4c08e925e0560cb270c91348e24c2d470c18
Anders Olme <anders.olme@gmail.com>
2011-09-08 07:29:21 +0200

    retab

--- bestbefore.py
+++ bestbefore.py
@@ -6,47 +6,47 @@ Created on 5 sep 2011
 import datetime, sys
 
 def isLeapYear(year):
-  retVal = year%4 == 0
-  if (year%4==0 and year%100==0 and not year%400==0):
-    retVal = False
-  return retVal
+    retVal = year%4 == 0
+    if (year%4==0 and year%100==0 and not year%400==0):
+        retVal = False
+    return retVal
 
 def convertToNumbers(datestring):
-  a,b,c= datestring.split('/')
-  return [int(a), int(b), int(c)]
+    a,b,c= datestring.split('/')
+    return [int(a), int(b), int(c)]
 
 def isDate(year,month,day):
-  retVal = False
-  try:
-    datetime.date(year,month,day)
-    retVal = True
-  except ValueError:
-    pass
-  return retVal
+    retVal = False
+    try:
+        datetime.date(year,month,day)
+        retVal = True
+    except ValueError:
+        pass
+    return retVal
 
 def lowestDate(indata):  
-  data=sorted(indata)
-  
-  if data[2] > 31:
-    data.insert(0,data.pop(2))
+    data=sorted(indata)
 
-  if data[0] < 2000:
-    data[0] = 2000 + data[0]
+    if data[2] > 31:
+        data.insert(0,data.pop(2))
 
-  if isDate(data[0], data[1], data[2]):
-    retVal = data
-  else:
-    retVal = [0,0,0]
+    if data[0] < 2000:
+        data[0] = 2000 + data[0]
 
-  return retVal
+    if isDate(data[0], data[1], data[2]):
+        retVal = data
+    else:
+        retVal = [0,0,0]
+
+    return retVal
 
 if __name__ == '__main__':
-  datestring = sys.stdin.readline()
+    datestring = sys.stdin.readline()
 
-  datearray = convertToNumbers(datestring.strip())
-  datearray=lowestDate(datearray)
-    
-  if datearray !=[0,0,0]:
-    print "{0}-{1:02d}-{2:02d}".format(datearray[0], datearray[1], datearray[2])
-  else:
-    print datestring.strip() + " is illegal" 
+    datearray = convertToNumbers(datestring.strip())
+    datearray=lowestDate(datearray)
+
+    if datearray !=[0,0,0]:
+        print "{0}-{1:02d}-{2:02d}".format(datearray[0], datearray[1], datearray[2])
+    else:
+        print datestring.strip() + " is illegal" 
--- bestbeforetest.py
+++ bestbeforetest.py
@@ -9,69 +9,68 @@ import bestbefore
 class Test(unittest.TestCase):
 
 
-  def setUp(self):
-    pass
+    def setUp(self):
+        pass
 
 
-  def tearDown(self):
-    pass
+    def tearDown(self):
+        pass
 
 
-  def testLeapyear(self):
-    self.assertTrue(bestbefore.isLeapYear(2000))
-    pass
+    def testLeapyear(self):
+        self.assertTrue(bestbefore.isLeapYear(2000))
+        pass
 
-  def testLeapyear100(self):
-    self.assertFalse(bestbefore.isLeapYear(2100))
-    pass
+    def testLeapyear100(self):
+        self.assertFalse(bestbefore.isLeapYear(2100))
+        pass
 
-  def testLeapyearNormal(self):
-    self.assertTrue(bestbefore.isLeapYear(2012))
-    pass
+    def testLeapyearNormal(self):
+        self.assertTrue(bestbefore.isLeapYear(2012))
+        pass
 
-  def testIsDate(self):
-    self.assertTrue(bestbefore.isDate(2011, 9, 5))
-    self.assertFalse(bestbefore.isDate(2011, 19, 5))
+    def testIsDate(self):
+        self.assertTrue(bestbefore.isDate(2011, 9, 5))
+        self.assertFalse(bestbefore.isDate(2011, 19, 5))
 
-  def testConvertToNumbers(self):
-    test = bestbefore.convertToNumbers("2011/01/12")
+    def testConvertToNumbers(self):
+        test = bestbefore.convertToNumbers("2011/01/12")
 
-    self.assertListEqual(test, [2011,1,12])
+        self.assertListEqual(test, [2011,1,12])
 
-  def testConvertToNumbersZeroYear(self):
-    test = bestbefore.convertToNumbers("0/01/12")       
-    self.assertListEqual(test, [0,1,12])
+    def testConvertToNumbersZeroYear(self):
+        test = bestbefore.convertToNumbers("0/01/12")       
+        self.assertListEqual(test, [0,1,12])
 
-    test = bestbefore.convertToNumbers("00/01/12")        
-    self.assertListEqual(test, [0,1,12])
+        test = bestbefore.convertToNumbers("00/01/12")        
+        self.assertListEqual(test, [0,1,12])
 
-    test = bestbefore.convertToNumbers("0000/01/12")        
-    self.assertListEqual(test, [0,1,12])
+        test = bestbefore.convertToNumbers("0000/01/12")        
+        self.assertListEqual(test, [0,1,12])
 
-  def testLowestDate(self):
-    self.assertListEqual(bestbefore.lowestDate([1,2,3]), [2001,2,3])
-    self.assertListEqual(bestbefore.lowestDate([3,2,1]), [2001,2,3])
-    self.assertListEqual(bestbefore.lowestDate([23,0,12]), [2000,12,23])
-    self.assertListEqual(bestbefore.lowestDate([31,5,2012]), [2012,5,31])
-    self.assertListEqual(bestbefore.lowestDate([30,5,12]), [2005,12,30])
-    self.assertListEqual(bestbefore.lowestDate([1,2,3]), [2001,2,3])
-    self.assertListEqual(bestbefore.lowestDate([1,0,1]), [2000,1,1])
+    def testLowestDate(self):
+        self.assertListEqual(bestbefore.lowestDate([1,2,3]), [2001,2,3])
+        self.assertListEqual(bestbefore.lowestDate([3,2,1]), [2001,2,3])
+        self.assertListEqual(bestbefore.lowestDate([23,0,12]), [2000,12,23])
+        self.assertListEqual(bestbefore.lowestDate([31,5,2012]), [2012,5,31])
+        self.assertListEqual(bestbefore.lowestDate([30,5,12]), [2005,12,30])
+        self.assertListEqual(bestbefore.lowestDate([1,2,3]), [2001,2,3])
+        self.assertListEqual(bestbefore.lowestDate([1,0,1]), [2000,1,1])
 
-  def testLowestDate1000(self):
-    self.assertListEqual(bestbefore.lowestDate([3,2002,1]), [2002,1,3])
+    def testLowestDate1000(self):
+        self.assertListEqual(bestbefore.lowestDate([3,2002,1]), [2002,1,3])
 
-  def testLowestDateLeap1(self):
-    self.assertListEqual(bestbefore.lowestDate([2,2200,29]), [0,0,0])
+    def testLowestDateLeap1(self):
+        self.assertListEqual(bestbefore.lowestDate([2,2200,29]), [0,0,0])
 
-  def testLowestDateLeap2(self):
-    self.assertListEqual(bestbefore.lowestDate([2,2000,29]), [2000,2,29])
+    def testLowestDateLeap2(self):
+        self.assertListEqual(bestbefore.lowestDate([2,2000,29]), [2000,2,29])
 
-  def testLowestDateSP2(self):
-    self.assertListEqual(bestbefore.lowestDate([31,9,73]), [0,0,0])
+    def testLowestDateSP2(self):
+        self.assertListEqual(bestbefore.lowestDate([31,9,73]), [0,0,0])
 
-
-  def testLowestDateSP1(self):
-    self.assertListEqual(bestbefore.lowestDate([2,4,67]), [2067,2,4])
+    def testLowestDateSP1(self):
+        self.assertListEqual(bestbefore.lowestDate([2,4,67]), [2067,2,4])
 if __name__ == "__main__":
-  #import sys;sys.argv = ['', 'Test.testName']
-  unittest.main()
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
